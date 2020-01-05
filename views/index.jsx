d4729c6430ab1a6414cafad9330df81d1c5f5e0b
Rahul Chauhan <rahcha36@in.ibm.com>
2020-01-05 12:54:01 +0530

    stt

---
+++ views/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LayoutAudio from './layoutAudio';
+import Demo from './demo';
+
+export default function Index() {
+  return (
+    <LayoutAudio>
+      <Demo />
+    </LayoutAudio>
+  );
+}
---
+++ views/layoutAudio.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Header, Jumbotron } from 'watson-react-components';
+
+
+export default function LayoutAudio({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>
+          Speech to Text Demo
+        </title>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
+        <link rel="stylesheet" href="/css/watson-react-components.min.css" />
+        <link rel="stylesheet" href="/css/style.css" />
+      </head>
+      <body>
+        
+        <div id="root">
+          {children}
+        </div>
+        <script type="text/javascript" src="scripts/bundle.js" />
+        <script type="text/javascript" src="https://cdn.rawgit.com/watson-developer-cloud/watson-developer-cloud.github.io/master/analytics.js" />
+      </body>
+    </html>
+  );
+}
+
+LayoutAudio.propTypes = {
+  children: PropTypes.object.isRequired, // eslint-disable-line
+};
